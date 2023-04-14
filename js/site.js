47ad4498adefa4e5432e8636a300d5e581c8e174
Rexkwondo <87598098+Rexkwondo@users.noreply.github.com>
2023-04-14 11:22:35 -0700

    created reverse string app

--- js/site.js
+++ js/site.js
@@ -1,12 +1,42 @@
-function displayMessage() {
-	let msg = document.getElementById('message').value;
-	//alert(msg);
+function getValues() {
 
-	Swal.fire(
-		{
-			backdrop: false,
-			title: 'App Name',
-			text: msg
-		}
-	);
+	let userInput = document.getElementById('message').value;
+
+	let reverseInput = reverseString(userInput);
+
+	displayString(reverseInput);
+
 }
+
+
+function reverseString(message) {
+
+	let reversedMessage = '';
+
+	for (let index = message.length - 1; index >= 0; index = index - 1) {
+		reversedMessage += message[index];
+	}
+
+	return reversedMessage;
+
+
+}
+
+
+function displayString(reversedMessage) {
+
+	document.getElementById('msg').textContent = reversedMessage;
+	document.getElementById('alert').classList.remove('d-none');
+
+
+}
+
+
+
+	// Swal.fire(
+	// 	{
+	// 		backdrop: false,
+	// 		title: 'JS-Rewind',
+	// 		text: msg
+	// 	}
+	// );
